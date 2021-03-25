c7ed7eab092d0bf20eb0033822f1d22c6226c82d
Isaac Sante <43215955+IsaacSante@users.noreply.github.com>
2021-03-24 21:16:40 -0400

    commit

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 const WebSocket = require('ws');
 const PORT = process.env.PORT || 3000;
-
 const arduinoSample = 'I am arduino data'
-
 const wss = new WebSocket.Server({ port: PORT });
 
 wss.on("connection", ws => {
-
-  console.log("New Client Connected");
-  ws.send('im the Heroku ws server');
+    console.log("New Client Connected");
+    ws.send('im the Heroku ws server');
 
   ws.on("message", data => {
     console.log(`Client has sent" + ${data}`)
     // ws.send("Your data is:" + data)
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send('other data:'+ data);
+        client.send(data);
       }
     });
-
   })
 
   ws.on("close", () => {
     console.log("Client has disconnected")
   })
-
 })
