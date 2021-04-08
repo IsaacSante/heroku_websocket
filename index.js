fcc8d9ccb53f9cea411d71936f2e8077b0048269
Isaac Sante <43215955+IsaacSante@users.noreply.github.com>
2021-04-08 01:41:41 -0400

    take quotes

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ wss.on("connection", ws => {
     // ws.send("Your data is:" + data)
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
+        data = data.replace(/['"]+/g, '')
         client.send(data);
       }
     });
